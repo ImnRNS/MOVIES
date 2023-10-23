9440237e9bf0c5cc56a976642a86212dfedbde74
IMAN <Imannursoleh610@gmail.com>
2023-10-23 22:13:18 +0700

    First Commit

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, jsonify
 import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
-
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
@@ -11,7 +10,7 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 MONGODB_URI = os.environ.get("MONGODB_URI")
-DB_NAME =  os.environ.get("DB_NAME")
+DB_NAME = os.environ.get("DB_NAME")
 
 client = MongoClient(MONGODB_URI)
 db = client[DB_NAME]
@@ -24,17 +23,14 @@ def home():
 
 @app.route("/movie", methods=["POST"])
 def movie_post():
-    # sample_receive = request.form['sample_give']
     url_receive = request.form['url_give']
-    star_receive = request.form['star_give']
+    star_receive = int(request.form['star_give'])  # Convert to int
     comment_receive = request.form['comment_give']
 
-    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     data = requests.get(url_receive, headers=headers)
 
     soup = BeautifulSoup(data.text, 'html.parser')
-
-    # From here on, we will write the code for extracting data from meta tags
 
     og_image = soup.select_one('meta[property="og:image"]')
     og_title = soup.select_one('meta[property="og:title"]')
@@ -43,7 +39,7 @@ def movie_post():
     image = og_image['content']
     title = og_title['content']
     description = og_description['content']
-    
+
     doc = {
         'image': image,
         'title': title,
@@ -54,7 +50,7 @@ def movie_post():
 
     db.movies.insert_one(doc)
 
-    return jsonify({'msg':'POST request!'})
+    return jsonify({'msg': 'POST request!'})
 
 @app.route("/movie", methods=["GET"])
 def movie_get():
